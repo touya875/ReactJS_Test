beeaff9825b3c01fe3cc1e47fdffdbd1fcaf6951
touya875 <ishiitouya@gmail.com>
2025-07-10 12:37:15 +0700

    second commit

--- src/services/customize-axios.jsx
+++ src/services/customize-axios.jsx
@@ -2,6 +2,9 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: "https://reqres.in",
+  headers: {
+    "x-api-key": "reqres-free-v1",
+  },
 });
 
 // Add a response interceptor
